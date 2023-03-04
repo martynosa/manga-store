96eccf7179581a980384e70b185cc618635cd5d0
martiN <78382341+martynosa@users.noreply.github.com>
2023-03-04 20:31:21 +0200

    using div for card

--- src/Components/Pages/Store/Card/Card.tsx
+++ src/Components/Pages/Store/Card/Card.tsx
@@ -1,19 +1,22 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import classes from './Card.module.css';
 // typescript
 import { IVolume } from '../../../../typescript/interfaces';
 
 const Card: React.FC<{ volume: IVolume }> = ({ volume }) => {
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    navigate(`/store/${volume.manga}/${volume.volume}`);
+  };
+
   return (
-    <Link
-      to={`/store/${volume.manga}/${volume.volume}`}
-      className={classes.card}
-    >
+    <div onClick={onClickHandler} className={classes.card}>
       <img src={volume.coverLink} alt="cover" />
       <p>
         <span>{volume.volume}</span> {volume.engVolumeName}
       </p>
-    </Link>
+    </div>
   );
 };
 
